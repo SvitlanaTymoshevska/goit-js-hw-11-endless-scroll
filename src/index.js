a2b4334cdc6d6cdb30a024cf004d96db2ce3a590
Svitlana Tymoshevska <amelia.tsa@gmail.com>
2022-11-18 16:15:39 +0200

    Added endless-scroll.

--- src/index.js
+++ src/index.js
@@ -6,22 +6,19 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const refs = {
     searchFormEl: document.querySelector('#search-form'),
     galleryEl: document.querySelector('.gallery'),
-    loadMoreWrapper: document.querySelector('.load-more-wrapper'),
-    loadMore: document.querySelector('.load-more'),   
 }
 const getingPhotos = new GetingPhotos();
 let lightbox;
 
 refs.searchFormEl.addEventListener('submit', onSubmit);
 refs.galleryEl.addEventListener('click', onImgClick);
-refs.loadMore.addEventListener('click', onLoadMore);
+window.addEventListener('scroll', onScrolling);
 
 async function onSubmit(e) { 
     e.preventDefault();
 
     if (refs.galleryEl.firstElementChild) {
         clearGallery();
-        refs.loadMoreWrapper.classList.add('is-hidden');
         getingPhotos.resetPage();
     }
 
@@ -33,25 +30,26 @@ async function onSubmit(e) {
         getingPhotos.increasePage();
         Notify.info(`Hooray! We found ${getingPhotos.totalHits} images.`); 
         lightbox = new SimpleLightbox('.gallery a');
-        refs.loadMoreWrapper.classList.remove('is-hidden');
     }
     catch {
         Notify.failure("Sorry, there are no images matching your search query. Please try again.");
     }  
 }
 
-async function onLoadMore(e) {
-    try {
-        const photos = await getingPhotos.getPhotos();
+async function onScrolling(e) { 
+    const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
 
-        makeGalleryMarkup(photos);
-        getingPhotos.increasePage();
-        lightbox.refresh();
-        smoothScrolling();
-    }
-    catch {
-        Notify.failure("We're sorry, but you've reached the end of search results.");
-        refs.loadMoreWrapper.classList.add('is-hidden');
+    if (scrollHeight - clientHeight === scrollTop) {
+        try {
+            const photos = await getingPhotos.getPhotos();
+
+            makeGalleryMarkup(photos);
+            getingPhotos.increasePage();
+            lightbox.refresh();
+        }
+        catch {
+            Notify.failure("We're sorry, but you've reached the end of search results.");
+        }
     }
 }
 
@@ -103,13 +101,4 @@ function onImgClick(e) {
 
 function clearGallery() { 
     refs.galleryEl.innerHTML = '';
-}
-
-function smoothScrolling() { 
-    const { height: cardHeight } = refs.galleryEl.firstElementChild.getBoundingClientRect();
-
-    window.scrollBy({
-    top: cardHeight * 2,
-    behavior: "smooth",
-    });
 }
